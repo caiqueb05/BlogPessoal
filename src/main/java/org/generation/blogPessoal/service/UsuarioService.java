def7e627ebbb33db6e2e0abb38a095eea769a9c9
Caique Bezerra <caique.bezerra@gmail.com>
2021-11-04 14:05:07 -0300

    @caiqueb05 [Task: Inserido foto e tipo e Usuario e userLonin Models]

--- src/main/java/org/generation/blogPessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -41,6 +41,8 @@ public class UsuarioService {
                 user.get().setToken(authHeader);
                 user.get().setId(usuario.get().getId());
                 user.get().setNome(usuario.get().getNome());
+                user.get().setFoto(usuario.get().getFoto());
+                user.get().setTipo(usuario.get().getTipo());
 
                 return user;
             }
